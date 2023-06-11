2e7ed2e9d54644821473be75c74cdbbb240da936
Tamani Phiri <tamanigabriel0@gmail.com>
2023-06-11 19:21:08 +0200

    prototype

--- index.js
+++ index.js
@@ -2,18 +2,43 @@ let express = require('express');
 let bodyparser = require('body-parser');
 const path = require('path');
 const multer = require('multer');
+const docxtopdf = require('docx-pdf')
 
 const app = express();
 
-app.use(express.static('./uploads'))
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.static('uploads'))
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+var storage=multer.diskStorage({
+    destination: function(req,file,cb){
+        cb( null, "uploads" );
+    },
+    filename: function(req,file,cb){
+        cb( null, Date.now()+path.extname(file.originalname));
+    }
+});
+
+var upload=multer({storage:storage});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/docxtopdf', (req, res) => {}); 
+app.post('/docxtopdf',upload.single('file'), (req, res) => {
+    console.log(req.file.path)
+    let outputfilepath=Date.now()+"output.pdf"
+    docxtopdf(req.file.path,outputfilepath,(err, res) => {
+        if (err){
+            console.log()
+        }      
+        else {
+            res.download(outputfilepath,()=>{
+                
+            })
+        }  
+    })
+}); 
 
 app.listen(5000,()=>{
     console.log('listening on port 5000');
