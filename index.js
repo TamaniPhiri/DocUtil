45a5ed451a6dcc8828aa9c598d27b58adedb6230
Tamani Phiri <tamanigabriel0@gmail.com>
2023-06-11 18:24:09 +0200

    changes being made

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 let express = require('express');
+let bodyparser = require('body-parser');
+const path = require('path');
+const multer = require('multer');
 
 const app = express();
 
+app.use(express.static('./uploads'))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-})
+});
+
+app.post('/docxtopdf', (req, res) => {}); 
 
 app.listen(5000,()=>{
     console.log('listening on port 5000');
