ebe9eb388b21e79b11f0c67436533745cf4db0e9
Tamani Phiri <tamanigabriel0@gmail.com>
2023-06-20 13:57:32 +0200

    pdf to word and excel successful successful

--- document_converter.py
+++ document_converter.py
@@ -27,9 +27,32 @@ def convert_pdf_to_doc(input_pdf, output_filename):
 
     return output_doc
 
+
+def convert_pdf_to_excel(input_pdf, output_filename):
+    # Create a temporary file to save the uploaded PDF content
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_filepath = temp_file.name
+        temp_file.write(input_pdf.read())
+
+    # Open PDF document from the temporary file
+    document = ap.Document(temp_filepath)
+
+    save_options = ap.ExcelSaveOptions()
+    save_options.format = ap.ExcelSaveOptions.ExcelFormat.XML_SPREAD_SHEET2003
+
+    # Save the file into MS Excel format with the desired output filename
+    output_excel = output_filename + ".xls"
+    document.save(output_excel, save_options)
+
+    # Delete the temporary file
+    os.remove(temp_filepath)
+
+    return output_excel
+
+
 def main():
-    st.title("Document Converter")
-    conversion_type = st.selectbox("Select Conversion Type", ("PDF to DOCX",))
+    st.title("DocUtils📁")
+    conversion_type = st.selectbox("Select Conversion Type", ("PDF to DOCX", "PDF to Excel"))
 
     if conversion_type == "PDF to DOCX":
         st.subheader("Convert PDF to DOCX")
@@ -44,6 +67,21 @@ def main():
                     progress_bar.progress(100)
                 st.success("Conversion Successful!")
                 st.download_button("Download DOCX", docx_file)
+
+    elif conversion_type == "PDF to Excel":
+        st.subheader("Convert PDF to Excel")
+        pdf_file = st.file_uploader("Upload PDF File", type=["pdf"])
+
+        if pdf_file is not None:
+            output_filename = pdf_file.name.rsplit(".", 1)[0]  # Extracting the filename without extension
+            if st.button("Convert"):
+                progress_bar = st.progress(0)
+                with st.spinner("Converting..."):
+                    excel_file = convert_pdf_to_excel(pdf_file, output_filename)
+                    progress_bar.progress(100)
+                st.success("Conversion Successful!")
+                st.download_button("Download Excel", excel_file)
+
 
 if __name__ == "__main__":
     main()
