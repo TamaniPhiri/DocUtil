de8c7226d504bc09e323ea56c79fac6cbd678c68
Tamani Phiri <tamanigabriel0@gmail.com>
2023-06-20 15:28:52 +0200

    working prototype

--- document_converter.py
+++ document_converter.py
@@ -27,7 +27,6 @@ def convert_pdf_to_doc(input_pdf, output_filename):
 
     return output_doc
 
-
 def convert_pdf_to_excel(input_pdf, output_filename):
     # Create a temporary file to save the uploaded PDF content
     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
@@ -49,10 +48,29 @@ def convert_pdf_to_excel(input_pdf, output_filename):
 
     return output_excel
 
+def convert_pdf_to_pptx(input_pdf, output_filename):
+    # Create a temporary file to save the uploaded PDF content
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_filepath = temp_file.name
+        temp_file.write(input_pdf.read())
+
+    # Open PDF document from the temporary file
+    document = ap.Document(temp_filepath)
+
+    save_options = ap.PptxSaveOptions()
+
+    # Save the file into MS PowerPoint format with the desired output filename
+    output_pptx = output_filename + ".pptx"
+    document.save(output_pptx, save_options)
+
+    # Delete the temporary file
+    os.remove(temp_filepath)
+
+    return output_pptx
 
 def main():
     st.title("DocUtils📁")
-    conversion_type = st.selectbox("Select Conversion Type", ("PDF to DOCX", "PDF to Excel"))
+    conversion_type = st.selectbox("Select Conversion Type", ("PDF to DOCX", "PDF to Excel", "PDF to PPTX"))
 
     if conversion_type == "PDF to DOCX":
         st.subheader("Convert PDF to DOCX")
@@ -65,6 +83,7 @@ def main():
                 with st.spinner("Converting..."):
                     docx_file = convert_pdf_to_doc(pdf_file, output_filename)
                     progress_bar.progress(100)
+
                 st.success("Conversion Successful!")
                 st.download_button("Download DOCX", docx_file)
 
@@ -77,11 +96,26 @@ def main():
             if st.button("Convert"):
                 progress_bar = st.progress(0)
                 with st.spinner("Converting..."):
-                    excel_file = convert_pdf_to_excel(pdf_file, output_filename)
+                    xls_file = convert_pdf_to_excel(pdf_file, output_filename)
                     progress_bar.progress(100)
-                st.success("Conversion Successful!")
-                st.download_button("Download Excel", excel_file)
 
+                st.success("Conversion Successful!")
+                st.download_button("Download XLS", xls_file)
+
+    elif conversion_type == "PDF to PPTX":
+        st.subheader("Convert PDF to PPTX")
+        pdf_file = st.file_uploader("Upload PDF File", type=["pdf"])
+
+        if pdf_file is not None:
+            output_filename = pdf_file.name.rsplit(".", 1)[0]  # Extracting the filename without extension
+            if st.button("Convert"):
+                progress_bar = st.progress(0)
+                with st.spinner("Converting..."):
+                    pptx_file = convert_pdf_to_pptx(pdf_file, output_filename)
+                    progress_bar.progress(100)
+
+                st.success("Conversion Successful!")
+                st.download_button("Download PPTX", pptx_file)
 
 if __name__ == "__main__":
     main()
